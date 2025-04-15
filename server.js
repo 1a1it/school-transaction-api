8a5580433ca2b879fcbefd7845d66c33d3916f98
1a1it <96414998+1a1it@users.noreply.github.com>
2025-04-16 03:53:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,30 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  'http://localhost:5173', // Local 
+  'https://transaction-fe.vercel.app/' 
+];
 
 app.use(cors({
-  origin: 'https://transaction-fe.vercel.app/', 
+  origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
   credentials: true
 }));
 
-// Import routes
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+
 const transactionsRouter = require("./routes/transactionRoutes");
 app.use("/transactions", transactionsRouter);
 
-// Import auth routes
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
 
